7ad9014313dcdcc167ac709b4c74f934488ab634
Sean Floyd <sfloy029@uottawa.ca>
2016-04-11 22:21:39 -0400

    Add IR presentation
    - add IR presentation redirect

--- app.rb
+++ app.rb
@@ -13,3 +13,7 @@ end
 get '/email' do
   redirect 'mailto:sfloy029@uottawa.ca'
 end
+
+get '/presentation' do
+  redirect 'https://docs.google.com/presentation/d/1Gq71N-WAg1CGG62_qHmoAqbYMGkIBgdEX6y90DXkbfI'
+end
