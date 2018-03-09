d55f2e956db96f7705d71ddb26ec009c290767d4
Sean Floyd <seanlouisfloyd@gmail.com>
2018-03-08 22:47:35 -0500

    presentations

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ get '/email' do
   redirect 'mailto:seanlouisfloyd@gmail.com'
 end
 
-get '/presentation' do
+get '/midterm' do
   redirect 'https://docs.google.com/presentation/d/1kH1qmdP0FqlPv3sCy3QjDG_x1mU7nph1Qt4iftV_BxU/edit?usp=sharing'
+end
+
+get '/demo' do
+  redirect 'https://docs.google.com/presentation/d/1YIpgIC0boEnh7EwCQHYGcSqJt1xKsU_dyHQFxFOO3so/edit?usp=sharing'
 end
