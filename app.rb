a025b9dc241cb4ca916889d2f90cd6a581b60ed9
Sean Floyd <seanlouisfloyd@gmail.com>
2018-10-18 15:52:04 -0400

    remove presentation links

--- app.rb
+++ app.rb
@@ -14,11 +14,3 @@ end
 get '/email' do
   redirect 'mailto:seanlouisfloyd@gmail.com'
 end
-
-get '/midterm' do
-  redirect 'https://docs.google.com/presentation/d/1kH1qmdP0FqlPv3sCy3QjDG_x1mU7nph1Qt4iftV_BxU/edit?usp=sharing'
-end
-
-get '/demo' do
-  redirect 'https://docs.google.com/presentation/d/1YIpgIC0boEnh7EwCQHYGcSqJt1xKsU_dyHQFxFOO3so/edit?usp=sharing'
-end
