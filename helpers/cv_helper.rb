cd7d3111860e3372eb79bef4cb7fe42d39f63cd4
Sean Floyd <seanfloyd@icloud.com>
2020-05-11 01:27:54 -0400

    [CV] add link to change lang.
    - add link to change language
    - do not show the link on print

--- helpers/cv_helper.rb
+++ helpers/cv_helper.rb
@@ -2,4 +2,15 @@ module CVHelper
   def icons
     {'GitHub' => 'github', 'StackOverflow' => 'stack-overflow', 'LinkedIn' => 'linkedin'}
   end
+
+  def change_language_path
+    href = request.fullpath
+    if R18n.get.locale.code === 'en'
+      '/fr' << href
+    elsif R18n.get.locale.code === 'fr'
+      href[3..-1]
+    else
+      ''
+    end
+  end
 end
